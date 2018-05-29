d82e5ae4b8a2a26f1c065ed34c70631532ebbd74
Ashikul Hosen <sagor.ashikul@gmail.com>
2018-05-29 12:52:24 +0600

    Updating ch11 on c__ primer

--- C__ Primer/ch11/example.cpp
+++ C__ Primer/ch11/example.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <set>
+
+
+// set example
+int main() {
+    
+    std::map<std::string, size_t> word_count;
+    std::set<std::string> exclude = {"The", "But", "And", "Or", "An", "A",
+                            "the", "but", "and", "or", "an", "a"};
+    
+    std::string word;
+    
+    while( std::cin >> word )
+        if( exclude.find(word) == exclude.end() )
+            ++word_count[word];
+        
+    for(const auto &w : word_count)
+        std::cout << w.first << " occurs " << w.second 
+        << ( (w.second > 1) ? " times" : " time") << std::endl; 
+        
+    return 0;
+
+}
 
 
 /* map example
+ * 
 int main() {
  
     std::map<std::string, size_t> word_count;
